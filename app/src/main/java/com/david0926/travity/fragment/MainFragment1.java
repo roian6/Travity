242746708b0ce0a6865d68d9b7142ae17226e57d
chlwhdtn03 <chlwhdtn03@naver.com>
2020-09-28 15:19:50 +0900

    Added Info RecyclerView

--- app/src/main/java/com/david0926/travity/fragment/MainFragment1.java
+++ app/src/main/java/com/david0926/travity/fragment/MainFragment1.java
@@ -5,16 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.david0926.travity.R;
 import com.david0926.travity.databinding.FragmentMain1Binding;
+import com.david0926.travity.model.InfoModel;
+import com.david0926.travity.util.InfoAdapter;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainFragment1 extends Fragment {
 
@@ -35,6 +42,13 @@ public class MainFragment1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main1, container, false);
+
+        ArrayList<InfoModel> infos = new ArrayList<>();
+        infos.add(new InfoModel("https://dimg.donga.com/wps/NEWS/IMAGE/2019/07/28/96733177.3.jpg", "★회사가 망했어요★ 비행기 증정"));
+        infos.add(new InfoModel("https://www.sangte.co.kr/web/product/big/201709/80_shop1_940482.jpg", "이벤트 안내 ^^"));
+
+        binding.infoRecyclerview.setAdapter(new InfoAdapter(infos));
+        binding.infoRecyclerview.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
 
         return binding.getRoot();
     }
---
+++ app/src/main/java/com/david0926/travity/util/InfoAdapter.java
@@ -0,0 +1,56 @@
+package com.david0926.travity.util;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.david0926.travity.R;
+import com.david0926.travity.model.InfoModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder> {
+    private ArrayList<InfoModel> list;
+
+    public class ViewHolder extends  RecyclerView.ViewHolder {
+        TextView tv;
+        ImageView iv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv = itemView.findViewById(R.id.info_title);
+            iv = itemView.findViewById(R.id.info_img);
+        }
+    }
+
+    public InfoAdapter(ArrayList<InfoModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_info, parent, false);
+        InfoAdapter.ViewHolder vh = new InfoAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(InfoAdapter.ViewHolder holder, int position) {
+        Picasso.get().load(Uri.parse(list.get(position).getImgsrc())).into(holder.iv);
+        holder.tv.setText(list.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
